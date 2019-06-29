f5ae08821fbb87047f0aa357014746f3f49e5557
Jan Kankovsky <52216257+kankovskyj@users.noreply.github.com>
2019-06-29 19:54:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
+
+$("#button2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
+
+$("#button3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Home").offset().top
+    }, 1000);
+});
+
+$("#button4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Services").offset().top
+    }, 2000);
+});
+
+$("#button5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Contact").offset().top
+    }, 2000);
+});
